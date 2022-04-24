c343233414936a8c4a853b7825ddabf79121c670
Ander <a.cardenas.quiros@gmail.com>
2022-04-24 18:56:29 -0500

    md5 en registro y ventana emergente al realizar registro

--- assets/js/registro.js
+++ assets/js/registro.js
@@ -1,4 +1,4 @@
-var txt_nombre, txt_apellido, txt_correo, txt_celular, txt_usuario, txt_contraseña, txt_confirmarcontra;
+var txt_nombre, txt_apellido, txt_correo, txt_celular, txt_usuario, txt_contraseña, txt_confirmarcontra,btn_cerrar_modal;
 let btn_submit;
 
 window.onload=function(){
@@ -11,7 +11,10 @@ window.onload=function(){
     txt_contraseña=document.getElementById("contraseña");
     txt_confirmarcontra=document.getElementById("c_contraseña");
     btn_submit=document.getElementById("enviar");
+	ventana = document.getElementById("ventana");
+	btn_cerrar_modal = document.getElementById("btn_cerrar_modal");
     btn_submit.addEventListener("click", evaluar);
+	btn_cerrar_modal.addEventListener("click",cerrarVentana);
 }
 function evaluar(){
     if(txt_contraseña.value != txt_confirmarcontra.value){
@@ -23,10 +26,11 @@ function evaluar(){
             correo: txt_correo.value, 
             celular: txt_celular.value,
             usuario: txt_usuario.value,
-            clave: txt_contraseña.value 
+            clave: md5(txt_contraseña.value) 
         }
 
         localStorage.setItem("usuario",JSON.stringify(usuario));
+        abrirVentana();
 
         txt_nombre.value ="";
         txt_apellido.value="";
@@ -37,4 +41,11 @@ function evaluar(){
         txt_confirmarcontra.value ="";
         
     }
+}
+function abrirVentana(){
+	ventana.classList.remove("hidden");
+}
+
+function cerrarVentana(evento){
+	location.href = "logeo.html";
 }
